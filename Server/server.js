cdd619299cfb74b67e22c39898d44e27475c892e
YassenAli <144853147+YassenAli@users.noreply.github.com>
2024-08-25 16:15:50 +0300

    change port number to 5000

--- Server/server.js
+++ Server/server.js
@@ -17,7 +17,7 @@ app.use('/api/borrows', borrowRoutes);
 app.use('/api/wishlists', wishlistRoutes);
 app.use(errorMiddleware);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
 });
 
